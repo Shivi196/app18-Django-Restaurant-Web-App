48de684535601f3a07e628f98bd1f9fa122feeb6
misha <misha07.sharma@gmail.com>
2025-04-09 20:23:51 +0530

    5th Commit : Jinja For loops

--- restaurant_menu/models.py
+++ restaurant_menu/models.py
@@ -3,28 +3,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Items(models.Model):
 
-
-    MEALS_TYPES = (
+MEAL_TYPE = (
         ("starters","Starters"),
         ("salads","Salads"),
         ("main dishes","Main Dishes"),
         ("desserts","Desserts")
-    )
+            )
 
-    STATUS = (
+STATUS = (
         (0,"Unavailable"),
         (1,"Available")
     )
+
+class Items(models.Model):
+
     meals = models.CharField(max_length=1000)
     meals_description = models.CharField(max_length=2000)
     price = models.DecimalField(decimal_places=5,max_digits=200)
-    meals_type = models.CharField(choices= MEALS_TYPES)
+    meals_type = models.CharField(choices= MEAL_TYPE)
     author_cook_user = models.ForeignKey(User,on_delete=models.PROTECT) #This jusify a many to one relationship btw the meals and cook , one cook can create many meals and update same on portal with admin interface
     status = models.IntegerField(choices = STATUS, default=1)
     date_create = models.DateTimeField(auto_now_add=True)
     date_update = models.DateTimeField(auto_now= True)
 
     def __str__(self):
-        self.meals
+        return  self.meals
+
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -4,12 +4,17 @@ from django.shortcuts import render
 # Class Based View
 
 from django.views import generic
-from .models import Items
+from .models import Items, MEAL_TYPE
 
 class MenuList(generic.ListView):
 
     queryset = Items.objects.order_by("-date_create")
     template_name = "index.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+        return context
 
 
 class MenuItemDetail(generic.DetailView):
